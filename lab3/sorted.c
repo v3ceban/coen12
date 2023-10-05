2903c2c9aee0ba43a99131ab75511602cb868ce0
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-10-04 20:56:08 -0700

    lab3 completed

--- lab3/sorted.c
+++ lab3/sorted.c
@@ -27,6 +27,7 @@ typedef struct set {
 static int search(SET *sp, char *elt, bool *found);
 
 // creates set, returns pointer to set
+// O(1)
 SET *createSet(int maxElts) {
   SET *sp;
   sp = malloc(sizeof(SET));
@@ -38,7 +39,8 @@ SET *createSet(int maxElts) {
   return sp;
 }
 
-// frees all set memory
+// deletes set and frees all memory
+// O(n), where n is number of elements in set (sp->count)
 void destroySet(SET *sp) {
   assert(sp != NULL);
   int i;
@@ -52,36 +54,44 @@ void destroySet(SET *sp) {
 }
 
 // returns the number of elements in set
+// O(1)
 int numElements(SET *sp) {
   assert(sp != NULL);
   return (sp->count);
 }
 
-// adds an element to the set and updates number of elements as count
+// adds an element to the set and updates number of elements as sp->count
+// Worst case (not considering search function): O(n), where n is number of elements in set (sp->count)
+// Worst case (considering search function): O(n*log(n))
 void addElement(SET *sp, char *elt) {
-  assert(sp != NULL);
+  assert(sp != NULL && elt != NULL);
   bool found;
   int idx, i;
+  // set idx as position of first duplicate in the set
   idx = search(sp, elt, &found);
   if (found == true) {
     return;
   }
   assert(sp->count < sp->length);
+  // shift all elements down starting at the end of the array up to idx
   for (i = sp->count; i > idx; i--) {
     sp->data[i] = sp->data[i - 1];
   }
+  // copy elt to set and update counter
   sp->data[idx] = strdup(elt);
   sp->count++;
   return;
 }
 
-// searches for a duplicate data in set, returns position of first duplicate or
-// -1
+// searches for a duplicate data in set, returns position of first duplicate or first empty spot in the set
+// Worst case: O(log(n)), where n is number of elements in set (sp->count)
 static int search(SET *sp, char *elt, bool *found) {
-  // assert(sp != NULL);
+  // don't need to assert for found as it's defined and used locally
+  assert(sp != NULL && elt != NULL);
   int lo, hi, mid, flag;
   hi = sp->count - 1;
   lo = 0;
+  //binary search
   while (lo <= hi) {
     mid = (hi + lo) / 2;
     flag = strcmp(elt, sp->data[mid]);
@@ -99,8 +109,10 @@ static int search(SET *sp, char *elt, bool *found) {
 }
 
 // removes an element from the set and updates count
+// Worst case (not considering search funciton): O(n), where n is number of elements in set (sp->count)
+// Worst case (considering search function): O(n*log(n))
 void removeElement(SET *sp, char *elt) {
-  assert(sp != NULL);
+  assert(sp != NULL && elt != NULL);
   bool found;
   int i, idx;
   idx = search(sp, elt, &found);
@@ -117,15 +129,18 @@ void removeElement(SET *sp, char *elt) {
   }
 }
 
-// looks for the element in set, returns its conetns if found or NULL otherwise
+// looks for the element in set, returns its contents if found or NULL otherwise
+// O(1) (not considering search)
+// O(log(n)) (considering search)
 char *findElement(SET *sp, char *elt) {
-  assert(sp != NULL);
+  assert(sp != NULL && elt != NULL);
   bool found;
   int pos = search(sp, elt, &found);
   return (found ? sp->data[pos] : NULL);
 }
 
 // copies all elements from set to a new memory location
+// O(1)
 char **getElements(SET *sp) {
   assert(sp != NULL);
   char **a;
