b75c5346ddaa26dfe12b39300083fa283544f315
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-10-09 19:36:00 -0700

    functions init

---
+++ lab3/strings/table.c
@@ -0,0 +1,59 @@
+/*
+ * File: table.c - strings
+ *
+ * Copyright:	2023, Vladimir Ceban
+ *
+ * Description: This file contains all the functions used in main for testing
+ * text files with various string functions See function definitions for more
+ * info
+ *
+ */
+
+#include "set.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// defines SET structure
+typedef struct set {
+  int count;
+  int length;
+  char **data;
+  char *flag;
+} SET;
+
+// defines search function to be used later
+static int search(SET *sp, char *elt, bool *found);
+
+SET *createSet(int maxElts){
+  return(printf("\n\nCREATE SET\n\n"));
+}
+
+void destroySet(SET *sp){
+  printf("\n\nDESTROY SET\n\n");
+  return;
+}
+
+int numElements(SET *sp){
+  return(printf("\n\nNUM ELEMENTS"));
+}
+
+void addElement(SET *sp, char *elt){
+  printf("\n\nADD ELEMENT\n\n");
+  return;
+}
+
+void removeElement(SET *sp, char *elt){
+  printf("\n\nREMOVE ELEMENT\n\n");
+  return;
+}
+
+char *findElement(SET *sp, char *elt) {
+  return(printf("\n\nFIND ELEMENT\n\n"));
+}
+
+char **getElements(SET *sp) {
+  return (printf("\n\nGET ELEMENTS\n\n"));
+}
