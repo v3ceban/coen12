d0cce581d8b73c4e02b0c0f691ab2bf1e838962a
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-10-18 22:50:11 -0700

    git structure updates

--- lab1/main.c
+++ lab1/main.c
@@ -6,44 +6,43 @@ Name: Vladimir Ceban
 
 */
 
-//Includes
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
 
-//Defines
+// Defines
 #define MAX_WORD_LENGTH 30
 int countFileWords(char *);
 int main(int, char *[MAX_WORD_LENGTH]);
 
-//Functions
-int countFileWords(char *fileName){
-   FILE *file;
-   int x = 0;
-   char word[MAX_WORD_LENGTH];
-   if ((file = fopen(fileName, "r")) == NULL) {
-      printf("File not found\n");
-      return -1;
-   }
-   else {
-      while (fscanf(file, "%s", word) == 1)
-      {
-         x++;
-      }
-      printf("%d", x);
-      return x;
-   }
+// Functions
+int countFileWords(char *fileName) {
+  FILE *file;
+  int x = 0;
+  char word[MAX_WORD_LENGTH];
+  if ((file = fopen(fileName, "r")) == NULL) {
+    printf("File not found\n");
+    return -1;
+  } else {
+    while (fscanf(file, "%s", word) == 1) {
+      x++;
+    }
+    printf("%d", x);
+    return x;
+  }
 }
 
-int main (int argc, char **argv) {
-   if (argc < 2) {
-      printf("Please specify a file to read. (i.e. ./countApp ./scrathc/Macbeth.txt)\n");
-      return 1;
-   }
-   if (countFileWords(argv[1])>=0) {
-      printf (" total words\n");
-      return 0;
-   } else {
-      printf("Your file couldn't be opened for some reason.\n");
-      return 1;
-   }
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("Please specify a file to read. (i.e. ./count "
+           "./scratch/Macbeth.txt)\n");
+    return 1;
+  }
+  if (countFileWords(argv[1]) >= 0) {
+    printf(" total words\n");
+    return 0;
+  } else {
+    printf("Your file couldn't be opened for some reason.\n");
+    return 1;
+  }
 }
