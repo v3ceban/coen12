76bfb4ecf93bf78055845bd71338ff100cdc7b85
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2023-11-01 22:35:22 -0700

    lab4 part 2 complete

--- lab4/list.c
+++ lab4/list.c
@@ -11,7 +11,7 @@
  *
  */
 
-// include headers and libraries
+// Include headers and libraries
 #include "list.h"
 #include <assert.h>
 #include <stdbool.h>
@@ -19,230 +19,243 @@
 #include <stdlib.h>
 #include <string.h>
 
-// define struct for list
+// Define struct for list
 typedef struct list {
+  // Number of items in the list
   int count;
-  // list containst the head that points to other nodes in the list
+  // List containst the head that points to other nodes in the list
   struct node *head;
-  // list also has a compare function implemented in driver main function
+  // List also has a compare function implemented in driver main function
   int (*compare)(void *, void *);
 } LIST;
 
-// define struct for node
+// Define struct for node
 typedef struct node {
   void *data;
-  // each node points to next and previous nodes in the list
+  // Each node points to next and previous nodes in the list
   struct node *next;
   struct node *prev;
 } NODE;
 
-// creates list pointer and returns a LIST pointer
+// Creates list pointer and returns a LIST pointer
 // Big O = O(1)
 LIST *createList(int (*compare)()) {
-  // allocate memory for the list and check that it worked out
+  // The assignment tells to not assert for compare here
+
+  // Allocate memory for the list and check that it worked out
   LIST *list = (LIST *)malloc(sizeof(LIST));
   assert(list != NULL);
 
-  // allocate memory for dummy node and make sure it allocated correctly
+  // Allocate memory for dummy node and make sure it allocated correctly
   NODE *head = (NODE *)malloc(sizeof(NODE));
   assert(head != NULL);
 
-  // assign list's head to created dummy node and set it to point to itself
-  // this helps to avoid empty head cases
+  // Assign list's head to created dummy node and set it to point to itself
+  // This helps to avoid empty head cases
   list->head = head;
   head->next = head->prev = head;
 
-  // set compare to function pointer and initialize the count
+  // Set compare to function pointer and initialize the count
   list->compare = compare;
   list->count = 0;
 
   return list;
 }
 
-// deletes the whole list and frees all memory
+// Deletes the whole list and frees all memory
 // Big O = O(lp->count)
 void destroyList(LIST *lp) {
   assert(lp != NULL);
 
   NODE *current;
-
-  // start at first node and cycle untill return to head
+  // Start at first node and cycle untill return to head
   for (current = lp->head->next; current != lp->head;
        current = lp->head->next) {
-    // move head->next to save the node
+    // Move head->next to save the node
     lp->head->next = current->next;
     free(current);
   }
-  // free head as it was not freed in the loop
+
+  // Free head, as it was not freed in the loop, and free list
   free(lp->head);
-  // free list to free all memory
   free(lp);
+
   return;
 }
 
-// return the number of nodes in the list
+// Return the number of nodes in the list
 // Big O = O(1)
 int numItems(LIST *lp) {
   assert(lp != NULL);
   return (lp->count);
 }
 
-// add a node as first element in the list
+// Add a node as first element in the list
 // Big O = O(1)
 void addFirst(LIST *lp, void *item) {
   assert(lp != NULL && item != NULL);
-  // allocate memory for new node and check allocations
+
+  // Allocate memory for new node and check allocations
   NODE *newNode = (NODE *)malloc(sizeof(NODE));
   assert(newNode != NULL);
-  // copy item to new node's data
+
+  // Copy item to new node's data
   newNode->data = item;
 
-  // shift pointers around to insert new node
+  // Shift pointers around to insert new node
   newNode->next = lp->head->next;
   newNode->prev = lp->head;
   lp->head->next->prev = newNode;
   lp->head->next = newNode;
 
-  // update counter
+  // Update counter
   lp->count++;
   return;
 }
 
-// add a node as last element in the list
+// Add a node as last element in the list
 // Big O = O(1)
 void addLast(LIST *lp, void *item) {
   assert(lp != NULL && item != NULL);
 
-  // allocate memory for new node and check allocation
+  // Allocate memory for new node and check allocation
   NODE *newNode = (NODE *)malloc(sizeof(NODE));
   assert(newNode != NULL);
 
-  // copy item to new node's data
+  // Copy item to new node's data
   newNode->data = item;
 
-  // shift pointers around to insert a new node
+  // Shift pointers around to insert a new node
   newNode->next = lp->head;
   newNode->prev = lp->head->prev;
   lp->head->prev->next = newNode;
   lp->head->prev = newNode;
 
-  // update counter
+  // Update counter
   lp->count++;
+
   return;
 }
 
-// delete first node in the list (not head, as it's a dummy node and doesn't
+// Delete first node in the list (not head, as it's a dummy node and doesn't
 // count). Big O = O(1)
 void *removeFirst(LIST *lp) {
   assert(lp != NULL && lp->count > 0);
-  // first node is after head
+
+  // First node is after head
   NODE *first = lp->head->next;
-  // copy data to return it
+  // Copy data to return it later
   void *data = first->data;
 
-  // shift pointers around to delete first node
+  // Shift pointers around to delete first node
   lp->head->next = first->next;
   first->next->prev = first->next;
 
-  // update counter and free node
+  // Update counter and free node
   lp->count--;
   free(first);
+
   return data;
 }
 
-// delete the last node in the list (also not head for the same reasons)
+// Delete the last node in the list (also not head for the same reasons)
 // Big O = O(1)
 void *removeLast(LIST *lp) {
   assert(lp != NULL && lp->count > 0);
-  // last node is before head
+
+  // Last node is before head
   NODE *last = lp->head->prev;
-  // copy data to return it
+  // Copy data to return it
   void *data = last->data;
 
-  // shift pointers around to delete last node
+  // Shift pointers around to delete last node
   lp->head->prev = last->prev;
   last->prev->next = last->next;
 
-  // update counter and free node
+  // Update counter and free node
   lp->count--;
   free(last);
+
   return data;
 }
 
-// find and return data from the first node in the list (not head as it's
+// Find and return data from the first node in the list (not head as it's
 // dummy) Big O = O(1)
 void *getFirst(LIST *lp) {
   assert(lp != NULL && lp->count > 0);
-  // as specified by instructions, list must not be empty
+  // As specified by instructions, list must not be empty
   // if it could've been empty, an if statement should've been used to
   // verify the condition and return NULL in case of empty list
   return (lp->head->next->data);
 }
 
-// find and return data from the last node in the list
+// Find and return data from the last node in the list
 // Big O = O(1)
 void *getLast(LIST *lp) {
   assert(lp != NULL && lp->count > 0);
-  // as specified by instructions, list must not be empty
+  // As specified by instructions, list must not be empty
   // if it could've been empty, an if statement should've been used to
   // verify the condition and return NULL in case of empty list
   return (lp->head->prev->data);
 }
 
-// find and remove (free) a node with data matching the given item
+// Find and remove (free) a node with data matching the given item
 // Big O = O(lp->count)
 void removeItem(LIST *lp, void *item) {
-  // check that lp and item are valid and that list is not empty
-  // assert(lp != NULL && item != NULL && lp->count > 0);
+  // Check that lp and item are valid and that list is not empty
   assert(lp != NULL && item != NULL && lp->count > 0);
+
   NODE *current;
-  // start searching from the first node and look until we return to the head
+  // Start searching from the first node and look until we return to the head
   for (current = lp->head->next; current != lp->head; current = current->next) {
     if (lp->compare(item, current->data) == 0) { // if node's data matches item
-      // shift pointers to delete node
+      // Shift pointers to delete node
       current->prev->next = current->next;
       current->next->prev = current->prev;
 
-      // delete node and update counter
+      // Delete node and update counter
       free(current);
       lp->count--;
       return;
     }
   }
+
   return;
 }
 
-// find and return node's data that matches the given item
+// Find and return node's data that matches the given item
 // Big O = O(lp->count)
 void *findItem(LIST *lp, void *item) {
-  // check that lp and item are valid and that list is not empty
+  // Check that lp and item are valid and that list is not empty
   assert(lp != NULL && item != NULL && lp->count >= 0);
+
   NODE *current;
-  // start searching from the first node and look until we return to the head
+  // Start searching from the first node and look until we return to the head
   for (current = lp->head->next; current != lp->head; current = current->next) {
     if (lp->compare(item, current->data) == 0) { // if node's data matches item
       return (current->data);
     }
   }
+
   return NULL;
 }
 
-// create and return an array of data from all nodes in the list
+// Create and return an array of data from all nodes in the list
 // Big O = O(lp->count)
 void *getItems(LIST *lp) {
-  // check that lp still exists
+  // Check that lp still exists
   assert(lp != NULL);
 
-  // allocate memory for the array and check that it was successful
+  // Allocate memory for the array and check that it was successful
   void **items = malloc(lp->count * sizeof(void *));
   assert(items != NULL);
 
   NODE *current;
   int idx = 0;
-  // loop through the array from first node untill we return back to head
+  // Loop through the array from first node untill we return back to head
   for (current = lp->head->next; current != lp->head;
        current = current->next, idx++) {
-    // copy data to items array
+    // Copy data to items array
     items[idx] = current->data;
   }
 
--- lab4/set.c
+++ lab4/set.c
@@ -19,89 +19,123 @@
 
 typedef struct set {
   int count;                              // number of elements in the set
-  int length;                             // max length of the list
+  int length;                             // max length of the list*
   LIST **list;                            // array of lists
   int (*compare)(void *elt1, void *elt2); // compare function provided by driver
   unsigned (*hash)(void *elt);            // hash function provided by driver
 } SET;
 
-// create set and return a pointer to it
+// Create set and return a pointer to the set
+// Big O = O(m) where m is the length of the set
 SET *createSet(int maxElts, int (*compare)(void *elt1, void *elt2),
                unsigned (*hash)(void *elt)) {
   assert(hash != NULL && compare != NULL);
+
   SET *sp = malloc(sizeof(SET));
   assert(sp != NULL);
-  // there are no elements when set is initialized
+
+  // There are no elements when set is initialized
   sp->count = 0;
-  // maximum maxElts/20 should be created
+  // *Maximum maxElts/20 should be created
   sp->length = maxElts / 20;
-  // allocate memory for all lists
+  // Allocate memory for all lists
   sp->list = malloc(sizeof(LIST *) * sp->length);
   assert(sp->list != NULL);
+  // Compare and hash pointers to driver functions
+  sp->compare = compare;
+  sp->hash = hash;
+
   int i;
-  // create maxElts/20 lists to optimize the search time
+  // *Create maxElts/20 lists to optimize the search time
   for (i = 0; i < sp->length; i++) {
     sp->list[i] = createList(compare);
   }
-  // compare and hash pointers to driver functions
-  sp->compare = compare;
-  sp->hash = hash;
+
   return sp;
 }
 
-// destroy all lists in the set, the set itself, and free memory
+// Destroy all lists in the set, the set itself, and free memory
+// Big O = O(n), where n is the sp->length
 void destroySet(SET *sp) {
   assert(sp != NULL);
+
   int i;
   for (i = 0; i < sp->length; i++) {
     destroyList(sp->list[i]);
   }
+
   free(sp->list);
   free(sp);
+
   return;
 }
 
-// just return the number of elements in the set in all lists
+// Just return the number of elements in the set in all lists
+// Big O = O(1)
 int numElements(SET *sp) {
   assert(sp != NULL);
+
   return (sp->count);
 }
 
+// Add new element to the beginning of the list that has the
+// hash of the added element. Big O = O(n) because of findItem,
+// where n is the length of the list
 void addElement(SET *sp, void *elt) {
   assert(sp != NULL && elt != NULL);
+
+  // Gets the index of the set that we need with external hash function
   int idx = sp->hash(elt) % sp->length;
   if (findItem(sp->list[idx], elt) == NULL) {
     addFirst(sp->list[idx], elt);
     sp->count++;
   }
+
+  return;
 }
 
+// Remove an element from the list that has the hash
+// of this item. Big O = O(n) because of removeItem, where
+// n is the length of the list
 void removeElement(SET *sp, void *elt) {
   assert(sp != NULL && elt != NULL);
+
+  // Gets the index of the set that we need with external hash function
   int idx = sp->hash(elt) % sp->length;
-  if (findItem(sp->list[idx], elt) != NULL) {
-    removeItem(sp->list[idx], elt);
-    sp->count--;
-  }
+  // Finds and removes the element, no need to find first
+  removeItem(sp->list[idx], elt);
+  sp->count--;
+
+  return;
 }
 
+// Find an element in the list that has the hash of this element
+// Big O = O(n) because of findItem, where n is the length of the list
 void *findElement(SET *sp, void *elt) {
   assert(sp != NULL && elt != NULL);
+
+  // Gets the index of the set that we need with external hash function
   int idx = sp->hash(elt) % sp->length;
+
   return (findItem(sp->list[idx], elt));
 }
 
+// Gets and returns all data from all lists present in the set
+// Big O = O(m), where m is the length of the set
 void *getElements(SET *sp) {
   assert(sp != NULL);
-  void **a;
-  int i;
-  a = malloc(sizeof(void *) * sp->count);
+
+  int i, index = 0;
+  void **a = malloc(sizeof(void *) * sp->count);
   assert(a != NULL);
-  int index = 0;
+
+  // Position to insert the new items in the array
   for (i = 0; i < sp->length; i++) {
-    void **items = getItems(sp->list[i]);
+    void *items = getItems(sp->list[i]);
     memcpy(a + index, items, sizeof(void *) * numItems(sp->list[i]));
+    // The new insertion happens at the end of previous
     index += numItems(sp->list[i]);
   }
+
   return a;
 }
